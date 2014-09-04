86647a013ac2d59640aa7feb63c2035849cd685e
thundervoice <dmbbandola@gmail.com>
2014-09-04 23:33:53 +0800

    modified expo.py

--- scripts/expo.py
+++ scripts/expo.py
@@ -1,8 +1,27 @@
+import cgi
+import json
+
 def make_exponentiater(e):
     return lambda(x): pow(x, e)
 
 square = make_exponentiater(2)
 cube = make_exponentiater(3)
 
-print square(4)
-print cube(4)
+def index(req, inp, pow):
+	inp = cgi.escape(inp)
+	pow = cgi.escape(pow)
+	n = int(inp)
+	e = int(pow)
+	
+	
+	if e == 2:
+		n = int(square(n))
+	elif e == 3:
+		n = int(cube(n))
+	
+	result = []
+	
+	result.append(str(n))
+
+
+	return json.dumps(result)
